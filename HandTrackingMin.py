1f899cfdf51258e785c1a08432e86f1dd78a702a
Albert Kuc <albert.kuc@gmail.com>
2021-06-03 00:39:33 +0100

    Update HandTrackingMin.py
     Add lines between hand points
     Include fps ratio to img
     Print hand points coordinates in terminal

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -12,6 +12,9 @@ hands = mpHands.Hands()
 # Draw points object
 mpDraw = mp.solutions.drawing_utils
 
+# 
+pTime = 0  # previous Time
+cTime = 0  # current Time
 
 while True:
     success, img = cap.read()  # that will give us our frame
@@ -26,8 +29,29 @@ while True:
     if results.multi_hand_landmarks:  # if detects look up object
 
         for handLms in results.multi_hand_landmarks:
-            # Extract information for each hand and display handLms in original image -> img 
-            mpDraw.draw_landmarks(img, handLms)
+
+            # Extract information for each hand 
+
+            # Get id number and ladnmark coordinates
+            for id, lm in enumerate(handLms.landmark):
+                # lm values are decimal places which is ratio of an image and not pixels
+                # we need to multiply it with h, w of the image to get the pixel value
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                print(id, cx, cy)
+
+            # Display handLms in original image -> img 
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    
+    # calculate frame rate
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    # include frame rate in img
+    cv2.putText(img, str(int(fps)), (10, 70),  # display in img, fps to int, at position,  
+                cv2.FONT_HERSHEY_PLAIN, 3,     # define font, define scale,
+                (255, 0, 255), 3)               # define color (purple), define thickness
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
